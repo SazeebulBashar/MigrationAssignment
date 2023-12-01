5d331feb2974cb2ab4471fd7c32d771fe21847a9
Sazeebul Bashar <87577411+SazeebulBashar@users.noreply.github.com>
2023-12-01 22:09:55 +0600

    assignment added

---
+++ database/migrations/2023_12_01_155807_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+
+            //relationship -no constrains were defined 
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
